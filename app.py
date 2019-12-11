04e68a83ec3872dc407a82609fccf0b4977227fa
sunggeun <topmaze@kaist.ac.kr>
2019-12-11 16:40:49 +0900

    add key_pos combined sequence interpreter

--- app.py
+++ app.py
@@ -25,18 +25,34 @@ def index():
 def request_suggestions_key():
     sequence = request.args.get('sequence')
     sequence = ''.join(sequence.split(','))
-    suggestions = gts.get_suggestions_from_key(sequence, 15)
+    suggestions = gts.get_suggestions_from_key(sequence, 10)
     return jsonify(suggestions)
 
 
 @app.route("/request/suggestions/position")
 def request_suggestions_position():
+    try:
+        sequence = request.args.get('sequence')
+        sequence = sequence.split(',')
+        positions = []
+        for i in range(0, len(sequence), 2):
+            positions.append([float(sequence[i]), float(sequence[i+1])])
+        suggestions = gts.get_suggestions_from_position(positions, 10)
+        return jsonify(suggestions)
+    except Exception:
+        return jsonify("")
+
+@app.route("/request/suggestions/key_pos")
+def request_suggestions_key_and_position():
     sequence = request.args.get('sequence')
-    sequence = sequence.split(',')
+    sequence = sequence.split(';')
+    positionTemp = sequence[1].split(',')
+    keys = []
+    keys = ''.join(sequence[0].split(','))
     positions = []
-    for i in range(0, len(sequence), 2):
-        positions.append([float(sequence[i]), float(sequence[i+1])])
-    suggestions = gts.get_suggestions_from_position(positions, 15)
+    for i in range(0, len(positionTemp), 2):
+        positions.append([float(positionTemp[i]), float(positionTemp[i+1])])
+    suggestions = gts.get_suggestions_from_keys_and_position(keys, positions, 10)
     return jsonify(suggestions)
 
 
