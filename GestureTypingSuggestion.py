62323e417202f6315e30c2ad3d6b54f39deaf0f1
leesangyoon <pinetree408@gmail.com>
2021-03-16 12:40:58 +0900

    Update dtw logic with specific length

--- GestureTypingSuggestion.py
+++ GestureTypingSuggestion.py
@@ -1,13 +1,19 @@
-import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
+
+import numpy as np
 import math
+import time
 import ray
+
+
 ray.init()
 
 
 class GestureTypingSuggestion():
     def __init__(self):
+        self.interpolation_size = 32
+
         self.keyboard_layout = [
             'qwertyuiop',
             'asdfghjkl',
@@ -18,39 +24,17 @@ class GestureTypingSuggestion():
         self.word_list = self.set_word_list()
         self.word_and_path_list = []
         for word, frequency in self.word_list:
-            word_path = self.convert_sequence_to_path(word)
-            self.word_and_path_list.append((word, frequency, word_path))
+            interpolated_word_path = self.convert_word_to_interpolated_path(word)
+            self.word_and_path_list.append((word, frequency, interpolated_word_path))
 
     def set_key_position(self):
         key_position = {}
         for row in range(len(self.keyboard_layout)):
             for col in range(len(self.keyboard_layout[row])):
                 char = self.keyboard_layout[row][col]
-                position = [col+0.5+row*0.5, row+0.5]
+                position = [col + 0.5 + row * 0.5, row + 0.5]
                 key_position[char] = position
         return key_position
-
-    def get_closest_keys_from_key(self, key):
-        key_list = []
-        point = self.key_position[key]
-        for char, value in self.key_position.items():
-            dist_x = math.pow(point[0]-value[0], 2)
-            dist_y = math.pow(point[1]-value[1], 2)
-            dist = math.sqrt(dist_x+dist_y)
-            if dist <= math.sqrt(2):
-                key_list.append(char)
-        return key_list
-
-    def get_closest_keys_from_position(self, position):
-        key_list = []
-        for char, value in self.key_position.items():
-            dist_x = math.pow(position[0]-value[0], 2)
-            dist_y = math.pow(position[1]-value[1], 2)
-            dist = math.sqrt(dist_x+dist_y)
-            if dist <= math.sqrt(2):
-                key_list.append([dist, char])
-        key_list.sort()
-        return [item[1] for item in key_list[:2]]
 
     def set_word_list(self):
         word_list = []
@@ -61,16 +45,123 @@ class GestureTypingSuggestion():
                 word_list.append((items[0], int(items[1])))
         return word_list
 
-    def convert_sequence_to_path(self, sequence):
-        path = []
-        for i in range(len(sequence)):
-            key = sequence[i]
+    def convert_word_to_path(self, word):
+        paths = []
+        for i in range(len(word)):
+            key = word[i]
+            key_position = self.key_position[key]
             if i > 0:
-                if key != sequence[i-1]:
-                    path.append(self.key_position[key])
+                if key != word[i - 1]:
+                    paths.append(key_position)
             else:
-                path.append(self.key_position[key])
-        return np.array(path)
+                paths.append(key_position)
+        return paths
+
+    def get_dist(self, pre, cur):
+        dist_x = math.pow(cur[0] - pre[0], 2)
+        dist_y = math.pow(cur[1] - pre[1], 2)
+        return math.sqrt(dist_x + dist_y)
+
+    def get_total_dist_from_path(self, paths):
+        total_dist = 0
+
+        if len(paths) > 1:
+            for i, path in enumerate(paths):
+                if i > 0:
+                    pre_path = paths[i - 1]
+                    dist = self.get_dist(pre_path, path)
+                    total_dist = total_dist + dist
+
+        return total_dist
+
+    def up_sampling(self, datas):
+        interpolated_datas = []
+
+        if len(datas) >= self.interpolation_size:
+            return interpolated_datas
+
+        if len(datas) > 1:
+            total_dist = self.get_total_dist_from_path(datas)
+            unit_dist = total_dist / (self.interpolation_size - 1)
+            temp_dist = 0
+            for i, cur_data in enumerate(datas):
+                if i > 0:
+                    pre_data = datas[i - 1]
+                    dist = self.get_dist(pre_data, cur_data)
+                    if temp_dist != 0:
+                        datas_num = int((dist - (unit_dist - temp_dist)) / unit_dist)
+                        ratio = (unit_dist - temp_dist) / dist
+                        start_data = [
+                            pre_data[0] + (cur_data[0] - pre_data[0]) * ratio,
+                            pre_data[1] + (cur_data[1] - pre_data[1]) * ratio,
+                        ]
+                        interpolated_datas.append(start_data)
+                        for j in range(datas_num):
+                            ratio = unit_dist * (j + 1) / dist
+                            data_x = start_data[0] + (cur_data[0] - pre_data[0]) * ratio
+                            data_y = start_data[1] + (cur_data[1] - pre_data[1]) * ratio
+                            interpolated_datas.append([data_x, data_y])
+                        temp_dist = (dist - (unit_dist - temp_dist)) - (datas_num * unit_dist)
+                    else:
+                        datas_num = int(dist / unit_dist)
+                        start_data = pre_data
+                        interpolated_datas.append(start_data)
+                        for j in range(datas_num):
+                            ratio = unit_dist * (j + 1) / dist
+                            data_x = start_data[0] + (cur_data[0] - pre_data[0]) * ratio
+                            data_y = start_data[1] + (cur_data[1] - pre_data[1]) * ratio
+                            interpolated_datas.append([data_x, data_y])
+                        temp_dist = dist - (datas_num * unit_dist)
+            if self.get_dist(datas[-1], interpolated_datas[-1]) > 0.001:
+                interpolated_datas.append(datas[-1])
+        elif len(datas) == 1:
+            for i in range(self.interpolation_size):
+                interpolated_datas.append(datas[0])
+
+        return interpolated_datas
+
+    def down_sampling(self, datas):
+        interpolated_datas = []
+
+        if len(datas) <= self.interpolation_size:
+            return interpolated_datas
+
+        total_dist = self.get_total_dist_from_path(datas)
+        unit_dist = total_dist / (self.interpolation_size - 1)
+        accum_dist = 0
+        min_dist = 1
+        min_dist_cand = [0, 0]
+        for i, cur_data in enumerate(datas):
+            if i == len(datas) - 1:
+                interpolated_datas.append(cur_data)
+            elif i > 0:
+                pre_data = datas[i - 1]
+                dist = self.get_dist(pre_data, cur_data)
+                accum_dist = accum_dist + dist
+                ratio = accum_dist / unit_dist
+                if abs(len(interpolated_datas) - ratio) < min_dist:
+                    min_dist = abs(len(interpolated_datas) - ratio)
+                    min_dist_cand = cur_data
+                else:
+                    interpolated_datas.append(min_dist_cand)
+                    min_dist = 1 
+            elif i == 0:
+                interpolated_datas.append(cur_data)
+
+        return interpolated_datas
+
+    def convert_word_to_interpolated_path(self, word):
+        paths = self.convert_word_to_path(word)
+
+        interpolated_paths = []
+        if len(paths) < self.interpolation_size:
+            interpolated_paths = self.up_sampling(paths)
+        elif len(paths) == self.interpolation_size:
+            interpolated_paths = paths
+        elif len(paths) > self.interpolation_size:
+            interpolated_paths = self.down_sampling(paths)
+
+        return np.array(interpolated_paths)
 
     @ray.remote
     def get_distance_from_dtw(target_path, word_path, word, frequency):
@@ -92,65 +183,36 @@ class GestureTypingSuggestion():
             result[0] = (alpha*r/sum_r)+((1-alpha)*n/sum_n)
         return results
 
-    def convert_position_to_path(self, position):
-        target_list = []
-        for i, point in enumerate(position):
-            min_dist = len(self.keyboard_layout)*len(self.keyboard_layout[0])
-            target_key = ''
-            for key, value in self.key_position.items():
-                dist_x = math.pow(point[0]-value[0], 2)
-                dist_y = math.pow(point[1]-value[1], 2)
-                dist = math.sqrt(dist_x+dist_y)
-                if min_dist > dist:
-                    min_dist = dist
-                    target_key = key
-            if len(target_list) != 0:
-                if target_list[len(target_list)-1][0] == target_key:
-                    if target_list[len(target_list)-1][2] > min_dist:
-                        target_list[len(target_list)-1] = [
-                            target_key, point, min_dist
-                        ]
-                else:
-                    target_list.append([target_key, point, min_dist])
-            else:
-                target_list.append([target_key, point, min_dist])
+    def convert_position_to_interpolated_path(self, position):
+        interpolated_paths = []
+        if len(position) < self.interpolation_size:
+            interpolated_paths = self.up_sampling(position)
+        elif len(position) == self.interpolation_size:
+            interpolated_paths = position
+        elif len(position) > self.interpolation_size:
+            interpolated_paths = self.down_sampling(position)
 
-        target = ''.join([target_item[0] for target_item in target_list])
-        target_path = [target_item[1] for target_item in target_list]
-        return (target, target_path)
+        return np.array(interpolated_paths)
 
-    def get_suggestions_from_position(self, poistion, suggest_num):
-        target, target_path = self.convert_position_to_path(poistion)
-        closest_key = self.get_closest_keys_from_position(target_path[-1])
+    def get_closest_keys_from_position(self, position):
+        key_list = []
+        for char, value in self.key_position.items():
+            dist = self.get_dist(position, value)
+            if dist <= math.sqrt(2):
+                key_list.append([dist, char])
+        key_list.sort()
+        return [item[1] for item in key_list[:2]]
+
+    def get_suggestions_from_position(self, position, suggest_num):
+        target_path = self.convert_position_to_interpolated_path(position)
+        closest_first_key = self.get_closest_keys_from_position(target_path[0])[0]
+        closest_last_keys = self.get_closest_keys_from_position(target_path[-1])
+
         target_word_and_path_list = list(
             filter(
                 lambda word_and_path: (
-                    (word_and_path[0][0] == target[0]) and
-                    (word_and_path[0][-1] in closest_key)
-                ),
-                self.word_and_path_list
-            )
-        )
-        results = ray.get([
-            self.get_distance_from_dtw.remote(
-                target_path, word_path,
-                word, frequency
-            )
-            for word, frequency, word_path in target_word_and_path_list
-        ])
-        results.sort()
-        results = self.get_score(results)
-        results.sort(reverse=True)
-        return results[:suggest_num]
-
-    def get_suggestions_from_key(self, target, suggest_num):
-        target_path = self.convert_sequence_to_path(target)
-        closest_key = self.get_closest_keys_from_key(target[-1])
-        target_word_and_path_list = list(
-            filter(
-                lambda word_and_path: (
-                    (word_and_path[0][0] == target[0]) and
-                    (word_and_path[0][-1] in closest_key)
+                    (word_and_path[0][0] == closest_first_key) and
+                    (word_and_path[0][-1] in closest_last_keys)
                 ),
                 self.word_and_path_list
             )
--- app.py
+++ app.py
@@ -21,14 +21,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/request/suggestions/key")
-def request_suggestions_key():
-    sequence = request.args.get('sequence')
-    sequence = ''.join(sequence.split(','))
-    suggestions = gts.get_suggestions_from_key(sequence, 10)
-    return jsonify(suggestions)
-
-
 @app.route("/request/suggestions/position")
 def request_suggestions_position():
     try:
@@ -36,27 +28,17 @@ def request_suggestions_position():
         sequence = sequence.split(',')
         positions = []
         for i in range(0, len(sequence), 2):
-            positions.append([float(sequence[i]), float(sequence[i+1])])
+            x = float(sequence[i])
+            y = float(sequence[i+1])
+            if len(positions) == 0:
+                positions.append([x, y])
+            else:
+                if positions[-1][0] != x or positions[-1][1] != y:
+                    positions.append([x, y])
         suggestions = gts.get_suggestions_from_position(positions, 10)
         return jsonify(suggestions)
     except Exception:
         return jsonify("")
-
-
-@app.route("/request/suggestions/key_pos")
-def request_suggestions_key_and_position():
-    sequence = request.args.get('sequence')
-    sequence = sequence.split(';')
-    positionTemp = sequence[1].split(',')
-    keys = []
-    keys = ''.join(sequence[0].split(','))
-    positions = []
-    for i in range(0, len(positionTemp), 2):
-        positions.append([float(positionTemp[i]), float(positionTemp[i+1])])
-    suggestions = gts.get_suggestions_from_keys_and_position(
-        keys, positions, 10
-    )
-    return jsonify(suggestions)
 
 
 if __name__ == '__main__':
